839d8e66e6f3cee5ece652c2661d777255ae8aea
Philipp Gramig <philipp.gramig@brickmakers.de>
2021-02-22 08:58:54 +0100

    Changed init

--- __init__.py
+++ __init__.py
@@ -2,15 +2,13 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
-class TestSkill(MycroftSkill):
-
-
+class RoomSkill(MycroftSkill):
     def __init__(self):
-        super(TestSkill, self).__init__(name="TestSkill")
+        MycroftSkill.__init__(self)
 
     @intent_handler(IntentBuilder("").require("Room"))
     def handle_room_intent(self, message):
         self.speak_dialog("room.is.free")
 
 def create_skill():
-    return TestSkill()
+    return RoomSkill()
